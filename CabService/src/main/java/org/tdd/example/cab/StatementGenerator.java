b8bac4b79a578153b968f86cf1650511efa1562b
Leydis Garzón Giro <leydis.garzon@gmail.com>
2020-10-12 07:52:56 -0500

    CabService test 2 pass

--- CabService/src/main/java/org/tdd/example/cab/StatementGenerator.java
+++ CabService/src/main/java/org/tdd/example/cab/StatementGenerator.java
@@ -3,6 +3,6 @@ package org.tdd.example.cab;
 public class StatementGenerator {
 
     public float calculateJournalFare(float distance, float time) {
-        return 0;
+        return time;
     }
 }

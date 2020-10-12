83011e04fbe151782b6681399777964f3458dc5e
Leydis Garzón Giro <leydis.garzon@gmail.com>
2020-10-12 08:42:51 -0500

    CabService test 4 pass

--- CabService/src/main/java/org/tdd/example/cab/controllers/StatementGenerator.java
+++ CabService/src/main/java/org/tdd/example/cab/controllers/StatementGenerator.java
@@ -1,8 +1,13 @@
 package org.tdd.example.cab.controllers;
 
+import org.tdd.example.cab.exceptions.CabException;
+
 public class StatementGenerator {
 
     public float calculateJournalFare(float kilometers, float minutes) {
+        if (minutes == 0 && kilometers != 0) {
+            throw new CabException("Time must be greater than zero.");
+        }
         return kilometers * 10 + minutes;
     }
 }

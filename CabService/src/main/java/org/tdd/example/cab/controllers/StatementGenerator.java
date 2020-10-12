36bc12e2e892a006804204e900596f431d0d55aa
Leydis Garzón Giro <leydis.garzon@gmail.com>
2020-10-12 08:54:32 -0500

    CabService test 7 pass

--- CabService/src/main/java/org/tdd/example/cab/controllers/StatementGenerator.java
+++ CabService/src/main/java/org/tdd/example/cab/controllers/StatementGenerator.java
@@ -5,11 +5,8 @@ import org.tdd.example.cab.exceptions.CabException;
 public class StatementGenerator {
 
     public float calculateJournalFare(float kilometers, float minutes) {
-        if (minutes < 0) {
-            throw new CabException("Time can't be negative.");
-        }
-        if (kilometers < 0) {
-            throw new CabException("Distance can't be negative.");
+        if (kilometers < 0 || minutes < 0) {
+            throw new CabException("Distance or time can't be negative.");
         }
         if (minutes == 0 && kilometers != 0) {
             throw new CabException("Time must be greater than zero.");

1adc14fc768c26da51948d1b82a1ddaeba87c50b
Leydis Garzón Giro <leydis.garzon@gmail.com>
2021-01-17 15:20:12 -0500

    RomanNumericConverter (initial commit).

---
+++ roman-numeric-converter/src/main/java/com/example/RomanNumericConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumericConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomanNumericConverterApplication.class, args);
+    }
+
+}
